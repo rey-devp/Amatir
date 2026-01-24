e59e5493aeff764333a093cc14c7a7d82e2cf886
rey-devp <rey.devp@gmail.com>
2026-01-24 21:33:03 +0700

    menambahkan koneksi ke db firebase dan menambahkan bukti gambar di model. cihuyy

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  try {
+    await dotenv.load(fileName: ".env");
+    print("✅ Environment variables loaded.");
+
+    await Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform,
+    );
+    print("✅ Firebase initialized successfully!");
+  } catch (e) {
+    print("❌ Failed to initialize Firebase: $e");
+  }
   runApp(const MyApp());
 }
 
